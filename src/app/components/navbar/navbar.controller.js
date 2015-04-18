0be8e863f8dcd6ea0cb6c522818c73ff9ae5c237
Filip Sobczak <filsob@gmail.com>
2015-04-18 17:00:34 +0100

    add yo files

---
+++ src/app/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('webAudioExperiment')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
---
+++ src/app/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.module('webAudioExperiment', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMaterial']);
